8d08c28292aa9ac0441163dcd4e99d55233a31d2
Dmitryy <llle8evvv@yandex.ru>
2021-05-22 09:01:42 +0300

    create app

---
+++ components/TextFeild.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-text-field v-model="message" solo label="Message" clearable>
+    </v-text-field>
+    <v-btn class="ma-2" outlined color="indigo" @click="post">
+      Отправить
+    </v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    async post() {
+      const message = this.message;
+      try {
+        await fetch("/api/api", {
+            'method': 'post',
+            message,
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+};
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app >
+    <v-navigation-drawer
+     
+      app
+      class="pt-4"
+      color="grey lighten-3"
+      mini-variant
+    >
+      <v-avatar
+        v-for="n in 6"
+        :key="n"
+        :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
+        :size="n === 1 ? 36 : 20"
+        class="d-block text-center mx-auto mb-9"
+      ></v-avatar>
+    </v-navigation-drawer>
+
+    <v-main>
+      <AppTextFeild />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppTextFeild from '@/components/TextFeild'
+export default {
+  components: {AppTextFeild},
+  data () {
+    return {
+    }
+  }
+}
+</script>
